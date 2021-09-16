5c0c76b020a7f6974209574209b1124ed7c771c9
JLUVicent <17390955615@163.com>
2021-09-16 19:05:44 +0800

    爬取当当网图片，书名，价格

---
+++ scrapy_dangdang_040/scrapy_dangdang_040/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ScrapyDangdang040Item(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # 通俗的说就是要下载的数据都要什么
+
+    pass
---
+++ scrapy_dangdang_040/scrapy_dangdang_040/spiders/dang.py
@@ -0,0 +1,39 @@
+import scrapy
+
+
+class DangSpider(scrapy.Spider):
+    name = 'dang'
+    allowed_domains = ['http://category.dangdang.com/cp01.01.02.00.00.00.html']
+    start_urls = ['http://category.dangdang.com/cp01.01.02.00.00.00.html']
+
+    def parse(self, response):
+        print("-----------------------------")
+
+        # pipelines  下载数据
+        # items      定义数据结构
+
+        # src='//ul[@id="component_59"]/li//img/@src'
+        # alt='//ul[@id="component_59"]/li//img/@alt'
+        # price='//ul[@id="component_59"]/li//span[@class="search_now_price"]/text()'
+        # 所有的seletor的对象都可以再次调用xpath方法
+        li_list = response.xpath('//ul[@id="component_59"]/li')
+        for li in li_list:
+            # 图片有懒加载，需要注意更换路径
+            # 第一张图片和其他标签的属性是不一样的
+            # 第一张图片的src可以使用，其他的图片的地址是data-original
+            src = li.xpath('.//img/@data-original').extract_first()
+            if src:
+                src = src
+            else:
+                src = li.xpath('.//img/@src').extract_first()
+
+            name = li.xpath('.//img/@alt').extract_first()
+            price = li.xpath(
+                './/span[@class="search_now_price"]/text()').extract_first()
+
+            print(src, name, price)
+
+        # price = response.xpath(
+        #     '//ul[@id="component_59"]/li//span[@class="search_now_price"]/text()')
+        # print(price.extract())
+        pass
